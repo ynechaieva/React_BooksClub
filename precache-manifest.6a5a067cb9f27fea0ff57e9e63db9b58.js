b9c342d285862dd782ab0629915b0573f2558ee3
ynechaieva <ynechaieva@outlook.com>
2020-06-28 14:05:43 +0200

    Updates

---
+++ precache-manifest.6a5a067cb9f27fea0ff57e9e63db9b58.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec904eabb9bbc3a50c89db5f291ada83",
+    "url": "/ynechaieva/React_BooksClub/index.html"
+  },
+  {
+    "revision": "3bac975e9e658e017d38",
+    "url": "/ynechaieva/React_BooksClub/static/css/2.92e352d9.chunk.css"
+  },
+  {
+    "revision": "2cee95c7b24dec6c8071",
+    "url": "/ynechaieva/React_BooksClub/static/css/main.bc88c057.chunk.css"
+  },
+  {
+    "revision": "3bac975e9e658e017d38",
+    "url": "/ynechaieva/React_BooksClub/static/js/2.377ac294.chunk.js"
+  },
+  {
+    "revision": "f2979e099ee4c06ef4b08e1c5db2b1ac",
+    "url": "/ynechaieva/React_BooksClub/static/js/2.377ac294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cee95c7b24dec6c8071",
+    "url": "/ynechaieva/React_BooksClub/static/js/main.814a7251.chunk.js"
+  },
+  {
+    "revision": "3db3250f2b6df48be278",
+    "url": "/ynechaieva/React_BooksClub/static/js/runtime-main.25cc02f2.js"
+  },
+  {
+    "revision": "ef00405606666fffa0a51614f9093cfb",
+    "url": "/ynechaieva/React_BooksClub/static/media/agree_thumb.ef004056.png"
+  },
+  {
+    "revision": "3d6f6618e1623da1cb4bc167287a79f1",
+    "url": "/ynechaieva/React_BooksClub/static/media/empty-star.3d6f6618.png"
+  },
+  {
+    "revision": "ab240bdf27e19dc620157d2fddaa6a64",
+    "url": "/ynechaieva/React_BooksClub/static/media/filled-star.ab240bdf.png"
+  },
+  {
+    "revision": "589e45f9f560b6dc3bf926fa3b0c9707",
+    "url": "/ynechaieva/React_BooksClub/static/media/sheriff.589e45f9.png"
+  }
+]);
